c9915dfdf66f0660ffd088e270f3272397c2c0ca
rae-daniel <ritadaniel740@gmail.com>
2023-08-11 16:58:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: eachtime 0 (successful)
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+
+	(C <= 'Z') {
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
