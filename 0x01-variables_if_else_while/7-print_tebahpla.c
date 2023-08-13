db682f53bc77cbf80af490ac7a2092488ce00c89
rae-daniel <ritadaniel740@gmail.com>
2023-08-13 10:13:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: eachtime 0 (successfull)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'z';
+	while
+		(b >= 'a') {
+			putchar(b);
+			b--;
+		}
+	putchar('\n');
+	return (0);
+}
