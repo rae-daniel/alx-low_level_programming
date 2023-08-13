b1ca247355a7525e9375ddc72137598a71706139
rae-daniel <ritadaniel740@gmail.com>
2023-08-13 10:40:28 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	char n;
+
+	int m;
+
+	n = 'a';
+	m = 0;
+	while
+		(m < 10) {
+			putchar(m + '0');
+			m++;
+		}
+	while
+		(n <= 'o') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
